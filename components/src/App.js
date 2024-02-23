21adf20571267fa2d6e53ab604379b3ffd128038
Juan Zambrano <jpablozambrano@hotmail.com>
2024-02-23 07:41:47 -0600

    completed activity 1

--- components/src/App.js
+++ components/src/App.js
@@ -1,10 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import Contact from './Contact'
-
+//app.js
+import { animals } from './animals';
+import ocean from './images/ocean.jpg';
 function App() {
-  return <Contact />;
+  //const title = 'hola';
+  const images = [];
+  const showBackground = true;
+  for (let animal in animals) {
+    images.push(
+      <img
+        key={animal}
+        className='animal'
+        alt={animal}
+        src={animals[animal].image}
+        ariaLabel={animal}
+        role='button'
+        onClick={displayFact}
+      />
+    );
+  }
+  function displayFact(e) {
+    const selectedAnimal = e.target.alt;
+    const animalInfo = animals[selectedAnimal];
+    const optionIndex = Math.floor(Math.random() * animalInfo.facts.length);
+    const funFact = animalInfo.facts[optionIndex];
+    document.getElementById('fact').innerHTML = funFact;
+  }
+  const title = '';
+  const animalFacts = (
+    <div>
+      <h1>{title ? title : 'Click an animal for a fun fact'}</h1>
+      
+      <p id='fact'></p>
+    </div>
+  );
+  const background = <img className='background' alt='ocean' src={ocean} />;
+  return (
+    <div>
+      {animalFacts}
+      {background}
+      <div className='animals'>{images}</div>
+    </div>
+  );
 }
 
-export default App
+export default App;
