bbbd46ea0583e9a6be40796955c11bad8d8bdc22
Juan Zambrano <jpablozambrano@hotmail.com>
2024-02-25 17:09:42 -0600

    Update Header.js

--- 3_component_interaction/src/Header.js
+++ 3_component_interaction/src/Header.js
@@ -1,2 +1,13 @@
+//Header.js
 import React from 'react';
 
+function Header(props) {
+    return (
+        <div>
+            <img src={props.profileImg} alt={props.profileImg} />
+            <h1>{props.username}</h1>
+        </div>
+    );
+}
+
+export default Header;
