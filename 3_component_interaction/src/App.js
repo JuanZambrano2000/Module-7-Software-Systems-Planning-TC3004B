28c5893697bd0b5f8e4ce7a3ed0a3b1a5f5e28f4
Juan Zambrano <jpablozambrano@hotmail.com>
2024-02-25 22:04:07 -0600

    App.js logic added

--- 3_component_interaction/src/App.js
+++ 3_component_interaction/src/App.js
@@ -1,2 +1,20 @@
+//App.js
 import React from 'react';
 import {comments} from './commentData';
+import {Card} from './Card';
+
+function App() {
+  return (
+    <div>
+      {comments.map((comment, index) => {
+        return (
+          <div key={index}>
+            <Card commentObject={comment}/>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
