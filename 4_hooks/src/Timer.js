598510fbebc0b5c92287522584f0c85f68f753e8
Juan Zambrano <jpablozambrano@hotmail.com>
2024-03-01 08:37:17 -0600

    Update Timer.js

--- 4_hooks/src/Timer.js
+++ 4_hooks/src/Timer.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 export default function Timer() {
   const [time, setTime] = useState(0);
+  const [name, setName] = useState('');
 
   useEffect(() => {
     const intervalID = setInterval(() => {
@@ -9,11 +10,16 @@ export default function Timer() {
     }, 1000);
     //clean up of the interval
     return () => {clearInterval(intervalID)};
-  });
+    //the dependency array is empty, so the effect runs only once
+  }, []);
+  
+  //when the input changes, the name state is updated, so the timer stops, so we will add a dependency array
+  const handleChange = ({ target }) => setName(target.value);
 
   return (
     <>
       <h1>Time: {time}</h1>
+      <input onChange={handleChange} value={name} />
     </>
   );
 }
