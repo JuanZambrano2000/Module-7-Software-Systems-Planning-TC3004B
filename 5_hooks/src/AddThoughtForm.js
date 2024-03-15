c2d79d5ebaa577e2b66cd0604b31bfd12486fc33
Juan Zambrano <jpablozambrano@hotmail.com>
2024-03-14 19:29:46 -0600

    it adds thoughts

--- 5_hooks/src/AddThoughtForm.js
+++ 5_hooks/src/AddThoughtForm.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { generateId, getNewExpirationTime } from './utilities';
 
 export function AddThoughtForm(props) {
+  const [thought, setThought] = useState('');
+
+  const handleTextChange = (event) => {
+    setThought(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    
+    const newThought = {
+      id: generateId(),
+      text: thought,
+      expiresAt: getNewExpirationTime(),
+    };
+    props.addThought(newThought);
+    setThought('');
+    
+  };
+
   return (
-    <form className="AddThoughtForm">
+    <form className="AddThoughtForm" onSubmit={handleSubmit}>
       <input
         type="text"
         aria-label="What's on your mind?"
         placeholder="What's on your mind?"
+        value={thought}
+        onChange={handleTextChange}
       />
       <input type="submit" value="Add" />
     </form>
